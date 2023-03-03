e73abf3ab98903b3be221b5b94832798c582e4e1
Rölz Johannes <Johannes.Roelz@BSLINZ2-IT.LOCAL>
2023-03-03 09:30:55 +0100

    make changes in ParcourRepository

--- Archery.Repository/ParcourRepository.cs
+++ Archery.Repository/ParcourRepository.cs
@@ -9,4 +9,5 @@ public class ParcourRepository : AbstractRepository
     public ParcourRepository(ArcheryContext context) : base(context) {}
 
     public IEnumerable<Parcour> GetAllParcours() => Context.Parcour.AsNoTracking().ToList();
+
 }
