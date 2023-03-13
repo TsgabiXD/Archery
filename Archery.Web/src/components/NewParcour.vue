577cbdc67a812708c0caa6a929c82080c09a8716
BSLINZ2-IT\Tobias.Schachner <schachner.tobias@gmail.com>
2023-03-13 12:36:50 +0100

    vue um start event erweitert

--- Archery.Web/src/components/NewParcour.vue
+++ Archery.Web/src/components/NewParcour.vue
@@ -5,22 +5,31 @@
       <v-container class="grey lighten-5" rounded>
         <v-row dense>
           <v-col cols="12" md="6">
-            <v-text-field label="Parcourname" outlined v-model=""> </v-text-field>
+            <v-text-field label="Parcourname" outlined v-model="parcourName">
+            </v-text-field>
           </v-col>
           <v-col cols="12" md="6">
-            <v-text-field label="Ort" outlined v-model=""> </v-text-field>
+            <v-text-field label="Ort" outlined v-model="location">
+            </v-text-field>
           </v-col>
         </v-row>
         <v-row dense>
           <v-col>
-            <v-text-field label="Anzahl der 3D-Tiere" outlined v-model=""> </v-text-field>
+            <v-text-field
+              label="Anzahl der 3D-Tiere"
+              outlined
+              v-model="animalCount"
+            >
+            </v-text-field>
           </v-col>
         </v-row>
       </v-container>
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="primary" elevation="2" @click=""> Hinzufügen </v-btn>
+      <v-btn color="primary" elevation="2" @click="addParcour">
+        Hinzufügen
+      </v-btn>
       <v-spacer></v-spacer>
     </v-card-actions>
   </v-card>
@@ -34,11 +43,25 @@ export default defineComponent({
   data: () => {
     return {
       isLoading: false,
+      parcourName: "",
+      location: "",
+      animalCount: 0,
     };
   },
   computed: {
     isLogin(): boolean {
       return true;
+    },
+  },
+  methods: {
+    addParcour(): void {
+      axios
+        .post("parcour/addparcour")
+        .then((response) => {
+          // TODO implement
+          response.data;
+        })
+        .catch((err) => console.log(err));
     },
   },
 });
--- Archery.Web/src/components/StartEventForm.vue
+++ Archery.Web/src/components/StartEventForm.vue
@@ -1,45 +1,63 @@
 <template>
-  <v-card elevation="7" :loading="isLoading" v-if="!isAddParcour">
-    <v-card-title> Neues Parcour-Event </v-card-title>
-    <v-card-text>
-      <v-container class="grey lighten-5" rounded>
-        <v-row dense>
-          <v-col>
-            <v-combobox
-              label="Parcour"
-              clearable
-              outlined
-              :items="parcours"
-              item-text="name"
-              v-model="selectedParcour"
-              hint="Alle angmeldeten Benutzer können hier mitspielen."
-            >
-            </v-combobox>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card-text>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn color="primary" elevation="2" @click="startEvent">
-        Event Starten
-      </v-btn>
-      <v-spacer></v-spacer>
-    </v-card-actions>
-  </v-card>
+  <div>
+    <v-card elevation="7" :loading="isLoading" v-if="!isAddParcour">
+      <v-card-title> Neues Parcour-Event </v-card-title>
+      <v-card-text>
+        <v-container class="grey lighten-5" rounded>
+          <v-row dense>
+            <v-col cols="12" md="6">
+              <v-text-field label="Parcourname" outlined v-model="eventName">
+              </v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-combobox
+                label="Parcour"
+                clearable
+                outlined
+                :items="parcours"
+                item-text="name"
+                v-model="selectedParcour"
+                hint="Alle angmeldeten Benutzer können hier mitspielen."
+                persistent-hint
+              >
+                <template v-slot:prepend-item>
+                  <v-btn tile depressed style="width: 100%" @click="addParcour"
+                    >+</v-btn
+                  >
+                </template>
+              </v-combobox>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" elevation="2" @click="startEvent">
+          Event Starten
+        </v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+    <new-parcour v-if="isAddParcour" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import NewParcour from "./NewParcour.vue";
 import axios from "@/router/axios";
 
 export default defineComponent({
+  components: {
+    NewParcour,
+  },
   data: () => {
     return {
       isLoading: false,
       isAddParcour: false,
       parcours: [], // TODO add Type
-      selectedParcour: {},
+      selectedParcour: "" as string, // TODO add Type
+      eventName: "",
     };
   },
   mounted() {
@@ -51,16 +69,19 @@ export default defineComponent({
       })
       .catch((err) => console.log(err));
   },
-  methods:{
-    startEvent(){
+  methods: {
+    startEvent(): void {
       axios
-      .post("parcour/addparcour")
-      .then((response) => {
-        // TODO implement
-        this.parcours = response.data;
-      })
-      .catch((err) => console.log(err));
-    }
-  }
+        .post("event/startevent")
+        .then((response) => {
+          // TODO implement
+          this.parcours = response.data;
+        })
+        .catch((err) => console.log(err));
+    },
+    addParcour(): void {
+      this.isAddParcour = true;
+    },
+  },
 });
 </script>
