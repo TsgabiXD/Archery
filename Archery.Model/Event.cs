93c426f4c86f351ddc88efaf86ae58bb175432bd
Walkner Luka <Luka.Walkner@BSLINZ2-IT.LOCAL>
2023-03-13 08:37:55 +0100

    add EventModel

---
+++ Archery.Model/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archery.Model
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;  
+        public Parcour Parcour { get; set; }= null!;
+        public List<User> User { get; set; } = new();
+        public bool IsRunning { get; set; }
+
+    }
+}
