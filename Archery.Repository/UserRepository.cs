6c2a90c9e85c8b2a7b5fbefefe0af6dd6810a8e2
Johannes Rölz <Johannes.Roelz@students.bs-linz2.ac.at>
2023-03-15 15:32:16 +0100

    TargetController, UserController BadRequest, ParcourRepo, TargetRepo, UserRepo trycatch

--- Archery.Repository/UserRepository.cs
+++ Archery.Repository/UserRepository.cs
@@ -10,23 +10,29 @@ public class UserRepository : AbstractRepository
     public IEnumerable<User> GetAllUsers()
     {
         return Context.User.AsNoTracking().ToList();
-        
+
     }
 
     public string AddUser(User user)
     {
-        if (!(string.IsNullOrEmpty(user.FirstName) && string.IsNullOrEmpty(user.LastName) && string.IsNullOrEmpty(user.NickName)))
+        if (user.FirstName.Length <= 150 && user.LastName.Length <= 150 && user.NickName.Length <= 150)
         {
-            Context.User.Add(user);
 
-            Context.SaveChanges();
-            return "success";
+            if (!(string.IsNullOrEmpty(user.FirstName) && string.IsNullOrEmpty(user.LastName) && string.IsNullOrEmpty(user.NickName)))
+            {
+                try
+                {
+                    Context.User.Add(user);
+                    Context.SaveChanges();
+                    return "Benutzer hinzugefügt";
+                }
+                catch (Exception ex)
+                {
+                    return "Fail: " + ex.Message;
+                }
+            }
+            return "Ungültige Werte!";
         }
-        return "fail";
+        return "Parcourname zu lang!";
     }
-
-    
-
-
-    
 }
