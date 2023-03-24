f06c248d67d6e5763bd300fa111ec6ee88c63ed8
BSLINZ2-IT\Tobias.Schachner <schachner.tobias@gmail.com>
2023-03-24 12:02:32 +0100

    overlay / dialog

---
+++ Archery.Web/src/components/NewTarget.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-overlay :value="show">
+    <!-- <v-dialog
+        v-model="dialog"
+        fullscreen
+        hide-overlay
+        transition="dialog-bottom-transition"
+      >
+      </v-dialog> -->
+  </v-overlay>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    show: { type: Boolean, required: true },
+  },
+});
+</script>
