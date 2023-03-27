b80c3abb7a65a0c87ed0d96c27da29c0bb407282
BSLINZ2-IT\Tobias.Schachner <schachner.tobias@gmail.com>
2023-03-27 12:42:39 +0200

    targetcontroller [Authorize] added

--- Archery.Api/Controllers/TargetController.cs
+++ Archery.Api/Controllers/TargetController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 using Archery.Model;
 using Archery.Repository;
 
 namespace Archery.Api.Controllers;
 
+
+[Authorize]
 [Route("api/[controller]")]
 public class TargetController : ArcheryController
 {
